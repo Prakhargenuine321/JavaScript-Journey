286ddf38db6580f6fcc9e22cb6c5af83425830af
Prakhar <prakhargenuine9336@gmail.com>
2024-07-15 00:04:00 +0530

    Another Commit

--- Lecture42/3scope.js
+++ Lecture42/3scope.js
@@ -14,6 +14,7 @@ if (true) {
 // console.log(c);
 
 
+//For nested function "inner-function" can access outer function members but outer function can't access inner function members.
 function one(){
     const username = "hitesh";
 
@@ -21,7 +22,7 @@ function one(){
         const website = "youtube";
         console.log(username);
     }
-    // console.log(website);
+    // console.log(website); //it will throw an error as it is out of it's scope.
 
     two();
 
@@ -44,6 +45,9 @@ if (true) {
 // ++++++++++++++++++ interesting ++++++++++++++++++
 
 
+//In below code there are two cases:
+//1)function using normal method -->here we can call function before declaration due to hoisting(where interpreter moved it's declaration to the top of an current scope).
+//2)funtion expression -->here we can't call function before declaration as it is referenced in "variable".
 console.log(addone(5));
 
 function addone(num){
