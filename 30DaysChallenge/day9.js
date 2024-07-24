70e9f24bde2dc5ae4891c4ce88134bf51796f938
Prakhar <prakhargenuine9336@gmail.com>
2024-07-24 13:42:42 +0530

    Another Commit

--- 30DaysChallenge/day9.js
+++ 30DaysChallenge/day9.js
@@ -22,3 +22,81 @@ const body = document.querySelector('body');
 const divEle = document.createElement('div.text');
 divEle.appendChild(document.createTextNode("I have been created using JS"));
 body.appendChild(divEle);
+
+
+//Task4
+
+const ul = document.querySelector('ul');
+
+const li = document.createElement('li');
+li.appendChild(document.createTextNode('Pranayam'));
+
+ul.appendChild(li);
+
+
+//Task5
+
+// li.remove();
+
+
+//Task6
+
+const lastChild = document.querySelector('li:last-child');
+lastChild.remove();
+
+
+//Task7
+
+const image = document.querySelector('.image');
+
+setInterval(function(){
+    image.style.backgroundImage = "url('https://images.unsplash.com/photo-1469719847081-4757697d117a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+} , 2000);
+
+
+//Task8
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click' , function(){
+    let image = document.querySelector('.image');
+    image.classList.remove('image');
+})
+
+const buttonOne = document.querySelector('.buttonOne')
+
+buttonOne.addEventListener('click' , function(){
+    let image = document.querySelector('.image-container > div')
+    image.classList.add('image');
+})
+
+
+//Task9
+
+const buttonTwo = document.querySelector('.button-two');
+
+const para = document.querySelector('p');
+
+buttonTwo.addEventListener('click' , function(){
+    para.innerText = "Hello Ji I changed a lot !";
+    para.style.fontFamily = "Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif";
+    para.style.fontSize = "2rem";
+})
+
+
+//Task10
+
+const border = document.querySelector('.border');
+
+// border.onmouseover = function(){
+//     border.style.borderColor = "black";
+// }
+
+border.addEventListener( 'mouseover' , function(){
+    border.style.borderColor = "black";
+})
+
+border.addEventListener( 'mouseout' , function(){
+    border.style.borderColor = "lightCoral";
+})
+
