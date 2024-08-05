e75a56c6f3c4228444bfb8c0d77c7788bd844949
Prakhar <prakhargenuine9336@gmail.com>
2024-08-05 19:44:24 +0530

    Usual Comit

---
+++ Lecture55/1promise.js
@@ -0,0 +1,11 @@
+//pehlae 'fetch' ki functionalities nahi thi tab 'Q' aur 'Blue-Bird' libraries ka use karke hum 'fetch' ki functionalities ka use karte thae.
+//baad maein in functionalities ko 'merge' kar diya gaya with JS.
+
+//Promises -->The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+//A Promise is in one of these states:
+//1) pending: initial state, neither fulfilled nor rejected.
+//2) fulfilled: meaning that the operation was completed successfully.
+//3) rejected: meaning that the operation failed.
+
+
+//learning how to create promises:-
